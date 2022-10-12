01c4d6921a6d0ed449be6c2e6dd81967bdaa7bf0
Jasonndlovu <Jasonndlovu42@gmail.com>
2022-10-12 12:49:36 +0200

    money allocation added

--- DisasterNPO/Models/DisasterModel.cs
+++ DisasterNPO/Models/DisasterModel.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.CodeAnalysis;
 
 namespace DisasterNPO.Models
 {
     public class DisasterModel
     {
+        MoneyModel model =  new MoneyModel();
+
         public int Id { get; set; }
         public string  Aidtypes { get; set; } = null!;
         public DateTime  StartDate{ get; set; }
@@ -13,7 +16,18 @@ namespace DisasterNPO.Models
 
         public string Description { get; set; } = null!;
 
+        public int Money_allocated { get; set; }
 
+
+        public int MoneyAllocation() { 
+            int allocation;
+            int value = Money_allocated;
+            int balance = model.Money;
+
+            allocation = balance - value;
+        
+            return allocation;
+        }
 
     }
 }
